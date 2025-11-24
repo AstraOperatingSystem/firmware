2e297d82194b4620c060dba5fbf79c91a76d5cc9
Didas72 <diogocruzdiniz@gmail.com>
2025-11-24 11:17:38 +0000

    Several tweaks to builds

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 mkdir build
-mkdir build/conan
 conan install . --profile conan_profiles/stm32f411re.txt -of build/conan --build=missing
 cd build
 cmake --toolchain conan/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release ..
