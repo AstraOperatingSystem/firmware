de3a091689519b8a4e3f16efd233753b052a6d9c
didas72 <diogocruzdiniz@gmail.com>
2025-12-16 22:57:57 +0000

    Testing and correcting keypad and display

--- app/src/main.c
+++ app/src/main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdint.h>
 
 #include <kpi/kpi.h>
@@ -14,16 +15,22 @@
 
 void keypad_handler(kpi_event_t ev)
 {
-	if (ev.type == KPI_EV_DOUBLE)
+	char buf[128];
+	int len = sprintf(buf, "%d %d\n", ev.button, ev.type);
+	HAL_UART_Transmit(&huart1, buf, len, 9999);
+
+	if (ev.type == KPI_EV_SHORT)
 		HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
 }
 
 void my_main()
 {
-	//display_t *display = display_init(&hi2c3, 0x27);
+	display_t *display = display_init(&hi2c3, 0x27 << 1);
+
+	display_write_str(display, "Hello");
 
 	kpi_bank_t bank;
-	kpi_bank_init_static(&bank, keypad_handler, 2, 500, 500);
+	kpi_bank_init_static(&bank, keypad_handler, 2, 400, 200);
 
 	while (1)
 	{
@@ -31,8 +38,8 @@ void my_main()
 
 		uint32_t btn_mask = 0;
 
-		btn_mask |= HAL_GPIO_ReadPin(BTN0_GPIO_Port, BTN0_Pin) << 0;
-		btn_mask |= HAL_GPIO_ReadPin(BTN1_GPIO_Port, BTN1_Pin) << 1;
+		btn_mask |= (1 - HAL_GPIO_ReadPin(BTN0_GPIO_Port, BTN0_Pin)) << 0;
+		btn_mask |= (1 - HAL_GPIO_ReadPin(BTN1_GPIO_Port, BTN1_Pin)) << 1;
 
 		kpi_tick(&bank, btn_mask);
 	}
